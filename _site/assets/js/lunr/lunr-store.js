d07bba93b6f653809eeadc83bc27e535eb58cb20
dongdongbh <18310682633@163.com>
2018-02-03 20:47:28 +0800

    update pages

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,9 +1,4 @@
 var store = [{
-        "title": "Markup: HTML Tags and Formatting",
-        "excerpt":"A variety of common markup showing how the theme styles them.Header twoHeader threeHeader fourHeader fiveHeader sixBlockquotesSingle line blockquote: Stay hungry. Stay foolish.Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on. But that’s not what it means at all....","categories": ["Markup"],
-        "tags": ["content","css","formatting","html","markup"],
-        "url": "http://localhost:4000/markup/markup-html-tags-and-formatting/",
-        "teaser":"http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"},{
         "title": "Dongdongbh, Launches Site",
         "excerpt":"Well. Finally got around to putting this old website together. Neat thing about it - powered by Jekyll and I can use Markdown to author my posts. It actually is a lot easier than I thought it was going to be.","categories": ["test"],
         "tags": ["content"],
@@ -13,4 +8,9 @@ var store = [{
         "excerpt":"a simple tutorial linkTable of contents setup git config basic command work with github branch merge UNDO merge and rebase subtreegithub add keyadd your github key file(id_rsa) to ~/.ssh/, then run following cmd in terminal:chmod 400 ~/.ssh/id_rsagit configglobal configgit config --global core.editor \"subl -n -w\" //change vi to sublgit config...","categories": [],
         "tags": ["git","github"],
         "url": "http://localhost:4000/git-tutorial/",
-        "teaser":null},]
+        "teaser":null},{
+        "title": "Markup: HTML Tags and Formatting",
+        "excerpt":"A variety of common markup showing how the theme styles them.Header twoHeader threeHeader fourHeader fiveHeader sixBlockquotesSingle line blockquote: Stay hungry. Stay foolish.Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on. But that’s not what it means at all....","categories": ["Markup"],
+        "tags": ["content","css","formatting","html","markup"],
+        "url": "http://localhost:4000/markup/markup-html-tags-and-formatting/",
+        "teaser":"http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"},]
