f8a1016226c976e73a4b62ccd827a9b544823864
dongdongbh <18310682633@163.com>
2018-02-21 23:23:23 +0800

    init coding

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,12 +5,17 @@ var store = [{
         "url": "http://localhost:4000/test/dongdong-site-launched/",
         "teaser":null},{
         "title": "git tutorial ",
-        "excerpt":"a simple tutorial linkTable of contents setup git config basic command work with github branch merge UNDO merge and rebase subtreegithub add keyadd your github key file(id_rsa) to ~/.ssh/, then run following cmd in terminal:chmod 400 ~/.ssh/id_rsagit configglobal configgit config --global core.editor \"subl -n -w\" //change vi to sublgit config...","categories": [],
-        "tags": ["git","github"],
+        "excerpt":"a simple tutorial link git tutorial Chinese versionTable of contents setup git config basic command work with github branch merge UNDO merge and rebase subtreegithub add keyadd your github key file(id_rsa) to ~/.ssh/, then run following cmd in terminal:chmod 400 ~/.ssh/id_rsagit configglobal configgit config --global core.editor \"subl -n -w\" //change...","categories": [],
+        "tags": ["git","tutorial"],
         "url": "http://localhost:4000/git-tutorial/",
         "teaser":null},{
         "title": "Markup: HTML Tags and Formatting",
         "excerpt":"A variety of common markup showing how the theme styles them.Header twoHeader threeHeader fourHeader fiveHeader sixBlockquotesSingle line blockquote: Stay hungry. Stay foolish.Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on. But that’s not what it means at all....","categories": ["Markup"],
-        "tags": ["content","css","formatting","html","markup"],
+        "tags": ["content","tutorial","formatting","html","markup"],
         "url": "http://localhost:4000/markup/markup-html-tags-and-formatting/",
+        "teaser":"http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"},{
+        "title": "Some useful resource links",
+        "excerpt":"Tutorials set  Ryan’s TutorialsTechnical  Linux Tutorial  Bash Scripting Tutorial  Regular ExpressionsPhilosophy  Problem Solving Skills  Graphic Design TutorialFor intersting  Rubik’s Cube Tutorial","categories": ["Resource"],
+        "tags": ["tutorial"],
+        "url": "http://localhost:4000/resource/useful-resource-link/",
         "teaser":"http://localhost:4000/assets/images/markup-syntax-highlighting-teaser.jpg"},]
